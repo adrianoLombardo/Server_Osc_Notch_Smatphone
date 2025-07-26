38102f9e1a90be83e22e28b18e68608d42ebf7c2
adrianoLombardo <69583218+adrianoLombardo@users.noreply.github.com>
2025-07-26 12:25:02 +0200

    fix runner restart with commonjs

--- runner.cjs
+++ runner.cjs
@@ -1,9 +1,5 @@
-import { spawn } from 'child_process';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const { spawn } = require('child_process');
+const path = require('path');
 
 const serverPath = path.join(__dirname, 'start.cjs');
 
