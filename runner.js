dce3ec1d5c1f30ce219c3aba846c18bbc46b3fe3
adrianoLombardo <69583218+adrianoLombardo@users.noreply.github.com>
2025-07-26 12:39:23 +0200

    fix run script for esm

--- runner.js
+++ runner.js
@@ -1,6 +1,9 @@
-const { spawn } = require('child_process');
-const path = require('path');
+import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const serverPath = path.join(__dirname, 'start.cjs');
 
 function launch() {
